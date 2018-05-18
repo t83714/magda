00072a798ed04ea8e0403e7aa98ecf1a03158ff6
Wang <wan273@csiro.au>
2018-05-18 14:14:53 +1000

    fix finding a publisher datasets problem

--- magda-dap-connector/aspect-templates/organization-details.js
+++ magda-dap-connector/aspect-templates/organization-details.js
@@ -1,5 +1,5 @@
 return {
-    name: 'CSIRO',
+    name: 'The Commonwealth Scientific and Industrial Research Organisation',
     identifier: 'CSIRO',
     title: 'The Commonwealth Scientific and Industrial Research Organisation',
     description: `The Commonwealth Scientific and Industrial Research Organisation (CSIRO) is Australia's national science agency and one of the largest and most diverse research agencies in the world. The CSIRO Data Access Portal provides access to research data, software and other digital assets published by CSIRO across a range of disciplines. The portal is maintained by CSIRO Information Management & Technology to facilitate sharing and reuse.`,
--- magda-dap-connector/src/Dap.ts
+++ magda-dap-connector/src/Dap.ts
@@ -201,9 +201,9 @@ export default class Dap implements ConnectorSource {
     getJsonFirstClassOrganization(id: string): Promise<any> {
         return Promise.resolve(
             {
-                name: 'CSIRO',
+                name: 'The Commonwealth Scientific and Industrial Research Organisation',
                 identifier: 'CSIRO',
-                title: 'CSIRO (Australia)',
+                title: 'The Commonwealth Scientific and Industrial Research Organisation (CSIRO)',
                 description: `The Commonwealth Scientific and Industrial Research Organisation (CSIRO) is Australia's national science agency and one of the largest and most diverse research agencies in the world. The CSIRO Data Access Portal provides access to research data, software and other digital assets published by CSIRO across a range of disciplines. The portal is maintained by CSIRO Information Management & Technology to facilitate sharing and reuse.`,
                 imageUrl: 'https://data.csiro.au/dap/resources-2.6.6/images/csiro_logo.png'
             }
@@ -223,9 +223,9 @@ export default class Dap implements ConnectorSource {
     getJsonDatasetPublisher(dataset: any): Promise<any> {
         return Promise.resolve(
             {
-                name: 'CSIRO',
+                name: 'The Commonwealth Scientific and Industrial Research Organisation',
                 identifier: 'CSIRO',
-                title: 'CSIRO (Australia)',
+                title: 'The Commonwealth Scientific and Industrial Research Organisation (CSIRO)',
                 description: `The Commonwealth Scientific and Industrial Research Organisation (CSIRO) is Australia's national science agency and one of the largest and most diverse research agencies in the world. The CSIRO Data Access Portal provides access to research data, software and other digital assets published by CSIRO across a range of disciplines. The portal is maintained by CSIRO Information Management & Technology to facilitate sharing and reuse.`,
                 imageUrl: 'https://data.csiro.au/dap/resources-2.6.6/images/csiro_logo.png'
             }
