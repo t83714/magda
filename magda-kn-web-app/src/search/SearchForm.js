b8b0d22ffd78545a9e7e6a556ec1984c2b177fea
Jonathan Yu <jonathan.yu@csiro.au>
2018-06-13 22:05:03 +1000

    tidy up some elements in frontend

--- magda-kn-web-app/src/search/SearchForm.js
+++ magda-kn-web-app/src/search/SearchForm.js
@@ -1,60 +1,73 @@
-import React, {Component} from 'react'
-import {Row, Col} from 'react-bootstrap'
+import React, { Component } from "react";
+import { Row, Col } from "react-bootstrap";
 
-import './SearchResult.css'
+import "./SearchResult.css";
 
 export default class SearchFrom extends Component {
-    constructor(props){
-        super(props)
-        this.state = {searchText: ''};
-        this.searchTextChange = this.searchTextChange.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
-      }
-
-    componentWillMount(){
-        if(this.props.match.params.text){
-            this.setState({searchText: this.props.match.params.text})
-        }
-        if(this.props.searchText){
-            this.setState({searchText: this.props.searchText})
-        }
-      }
-
-    searchTextChange(e){
-        this.setState({searchText: e.target.value})
+    constructor(props) {
+        super(props);
+        this.state = { searchText: "" };
+        this.searchTextChange = this.searchTextChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleSubmit(e){
-        e.preventDefault()
-        if(this.props.updateSearchStatus){
-            this.props.updateSearchStatus(this.state.searchText)
+    componentWillMount() {
+        if (this.props.match.params.text) {
+            this.setState({ searchText: this.props.match.params.text });
         }
-        this.props.history.push('/search/'+this.state.searchText)
+        if (this.props.searchText) {
+            this.setState({ searchText: this.props.searchText });
+        }
     }
 
-    render(){
+    searchTextChange(e) {
+        this.setState({ searchText: e.target.value });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        if (this.props.updateSearchStatus) {
+            this.props.updateSearchStatus(this.state.searchText);
+        }
+        this.props.history.push("/search/" + this.state.searchText);
+    }
+
+    render() {
         return (
             <Row>
-                <Col md={ 3}>
-                    <img className="img-responsive img-rounded" src="/img/knowledge_graph1.png" width="200px" alt="" />
+                <Col md={3}>
+                    <img
+                        className="img-responsive img-rounded"
+                        src="/img/knowledge_graph1.png"
+                        width="200px"
+                        alt=""
+                    />
                 </Col>
-                <Col md={ 9} >
-                    {/* <form role="search" onSubmit={this.handleSubmit}> */}
+                <Col md={9}>
+                    <form role="search" onSubmit={this.handleSubmit}>
                         <div className="search-bar input-group">
-                            <input type="text" className="search-query form-control" placeholder="Search for data or geospatial features... e.g. Namoi" 
-                                name="q" 
-                                value = {this.state.searchText}
+                            <input
+                                type="text"
+                                autocomplete="off"
+                                className="search-query form-control"
+                                placeholder="Search for data ... e.g. water"
+                                name="q"
+                                value={this.state.searchText}
                                 onChange={this.searchTextChange}
                             />
                             <span className="input-group-btn">
-                            <button className="btn btn-danger" type="submit" onClick={this.handleSubmit}>
-                                <span className=" glyphicon glyphicon-search"></span>
-                            </button>
+                                <button
+                                    className="btn btn-danger"
+                                    type="submit"
+                                    onClick={this.handleSubmit}
+                                >
+                                    <span className=" glyphicon glyphicon-search" />
+                                </button>
                             </span>
                         </div>
-                    {/* </form> */}
+                    </form>
                 </Col>
             </Row>
-        )
+        );
     }
 }
