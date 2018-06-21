4db633b6e421f45253dc6ded80eb2e640fe2c3b1
Wang <wan273@csiro.au>
2018-06-21 16:27:47 +1000

    fix dataset empty description bug

--- magda-kn-web-app/src/search/SearchResultView.js
+++ magda-kn-web-app/src/search/SearchResultView.js
@@ -1,35 +1,60 @@
-import React, {Component} from 'react'
-import { Link} from 'react-router-dom'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
-export default class SearchResultView extends Component{
-
-    render(){
+export default class SearchResultView extends Component {
+    render() {
         // console.log(this.props.result)
-        if(this.props.result!=='' && this.props.result !== undefined){
-            return(
+        if (this.props.result !== "" && this.props.result !== undefined) {
+            return (
                 <div>
-                        {this.props.result.dataSets.map((item, key) => {
-                            // console.log(item.publisher)
-                            var publisher_identifier = item.publisher? encodeURIComponent(item.publisher.identifier):encodeURIComponent(item.identifier)
-                            return (
-                                <div className="search-res-item" key={key}>
-                                
-                                    <h3> {key+1}. <Link to={'/dataset/'+ publisher_identifier + '/'+ encodeURIComponent(item.identifier)} > {item.title} </Link></h3>
-                                    <div className="search-res-row">
-                                        <div><cite>{item.landingPage}</cite></div>
-                                        <span className="search-res-text">{item.description.substring(0,200)}</span>
-                                        <div className="search-res-text"><b>Source:</b>{item.catalog}</div>
-                                        <div className="search-res-text"><b>Score:</b>{item.quality}</div>
+                    {this.props.result.dataSets.map((item, key) => {
+                        // console.log(item.publisher)
+                        var publisher_identifier = item.publisher
+                            ? encodeURIComponent(item.publisher.identifier)
+                            : encodeURIComponent(item.identifier);
+                        return (
+                            <div className="search-res-item" key={key}>
+                                <h3>
+                                    {" "}
+                                    {key + 1}.{" "}
+                                    <Link
+                                        to={
+                                            "/dataset/" +
+                                            publisher_identifier +
+                                            "/" +
+                                            encodeURIComponent(item.identifier)
+                                        }
+                                    >
+                                        {" "}
+                                        {item.title}{" "}
+                                    </Link>
+                                </h3>
+                                <div className="search-res-row">
+                                    <div>
+                                        <cite>{item.landingPage}</cite>
                                     </div>
-                                    <hr/>
+                                    <span className="search-res-text">
+                                        {item.description
+                                            ? item.description.substring(0, 200)
+                                            : ""}
+                                    </span>
+                                    <div className="search-res-text">
+                                        <b>Source:</b>
+                                        {item.catalog}
+                                    </div>
+                                    <div className="search-res-text">
+                                        <b>Score:</b>
+                                        {item.quality}
+                                    </div>
                                 </div>
-                            )
-                        })}
-                    </div>
-            )
-        }else{
-            return <div></div>
+                                <hr />
+                            </div>
+                        );
+                    })}
+                </div>
+            );
+        } else {
+            return <div />;
         }
-        
     }
 }
