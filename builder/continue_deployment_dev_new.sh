26dbc1c429e92a1bb92776f7e3dfa809ac91d40e
Jevy Wang <jevy.wangfei@gmail.com>
2018-07-12 16:02:26 +1000

    add staging deploy config

--- builder/continue_deployment_dev_new.sh
+++ builder/continue_deployment_dev_new.sh
@@ -2,7 +2,9 @@
 
 NAMESPACE=$1
 TAG=$2
-
+# update helm deploy config file following document at https://confluence.csiro.au/display/OFW/KN2+Magda+Prod+Deployment+on+Google+Kubernetes+Engine
+# magda-dev-kn-v0.0.41.yml
+HELM_CONFIG_FILE=$3
 GCLOUD_CLUSTER=kn-dev-cluster-1
 GCLOUD_ZONE=australia-southeast1-a
 GCLOUD_PROJECT=knowledge-network-192205
@@ -11,8 +13,7 @@ GCLOUD_USER=kn-dev-robot
 # Download the key, and put it at root directory of magda project
 GCLOUD_KEY_FILE=kn-dev-robot.json
 
-# update helm deploy config file following document at https://confluence.csiro.au/display/OFW/KN2+Magda+Prod+Deployment+on+Google+Kubernetes+Engine
-HELM_CONFIG_FILE=magda-dev-kn-v0.0.41.yml
+
 # View the page above to learn how to apply and config Google OAuth
 GOOGLE_CLIENT_SECRET='Vnw5fxIwXMOEpYpTvWggtzpc' 
 FACEBOOK_CLIENT_SECRET='p4ssw0rd'
@@ -26,13 +27,9 @@ gcloud auth activate-service-account $GCLOUD_USER@$GCLOUD_PROJECT.iam.gserviceac
 gcloud container clusters get-credentials $GCLOUD_CLUSTER --zone $GCLOUD_ZONE --project $GCLOUD_PROJECT
 
 kubectl config set-context $(kubectl config current-context) --namespace=$NAMESPACE
-if kubectl get namespace|grep $NAMESPACE >/dev/null
-then
-    $NAMESPACE_EXIST=true
-    cd /app && git add . && git commit -m "commit for pull" && git pull
-else
-    kubectl create namespace $NAMESPACE
-fi
+
+cd /app && git add . && git commit -m "commit for pull" && git pull
+
 
 cd /app  && yarn install && lerna link
 cd /app/magda-web-server && lerna run --loglevel=debug --scope=@magda/typescript-common --scope=@magda/web-admin --scope=@magda/web-server --scope=@magda/web-client --scope=@magda/kn-web-app* build
@@ -51,23 +48,5 @@ gcloud docker -- push gcr.io/$GCLOUD_PROJECT/data61/magda-web-server:$TAG
 gcloud docker -- push gcr.io/$GCLOUD_PROJECT/data61/magda-gateway:$TAG
 #gcloud docker -- push gcr.io/$GCLOUD_PROJECT/data61/magda-dap-connector:$TAG
 
-if [ $NAMESPACE_EXIST = true ]
-then
-    cd /app/deploy/helm && helm upgrade $NAMESPACE magda  --set web-server.image.repository="gcr.io/$GCLOUD_PROJECT/data61" --set web-server.image.tag=$TAG --set gateway.image.repository="gcr.io/$GCLOUD_PROJECT/data61" --set gateway.image.tag=$TAG -f $HELM_CONFIG_FILE
-else 
-    # Init helm
-    kubectl create serviceaccount --namespace kube-system tiller
-    kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
-    # kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'     
-    helm init --service-account tiller --upgrade
-    # Create db secret
-    kubectl create secret generic db-passwords --from-literal=combined-db=p4ssw0rd --from-literal=authorization-db=p4ssw0rd --from-literal=discussions-db=p4ssw0rd --from-literal=session-db=p4ssw0rd --from-literal=registry-db=p4ssw0rd --from-literal=combined-db-client=p4ssw0rd --from-literal=authorization-db-client=p4ssw0rd --from-literal=discussions-db-client=p4ssw0rd --from-literal=session-db-client=p4ssw0rd --from-literal=registry-db-client=p4ssw0rd --from-literal=elasticsearch=p4ssw0rd
-    # Create sessionsecret
-    cd /app/deploy/helm && ./create-auth-secrets.sh
-    # Create oAuth secret
-    # For how to create aaf client uri and configure google oAuth, please reference the appendix
-    kubectl create secret generic oauth-secrets --from-literal=google-client-secret=$GOOGLE_CLIENT_SECRET --from-literal=facebook-client-secret=$FACEBOOK_CLIENT_SECRET --from-literal=aaf-client-secret=$AAF_CLIENT_SECRET
-    # Install using helm
-    cd /app/deploy/helm && helm install --timeout 999999999 --name $NAMESPACE magda  --set web-server.image.repository="gcr.io/knowledge-network-prod/data61" --set web-server.image.tag=$TAG --set gateway.image.repository="gcr.io/knowledge-network-prod/data61" --set gateway.image.tag=$TAG -f magda-prod-kn-slim.yml
 
-fi
+cd /app/deploy/helm && helm upgrade $NAMESPACE magda  --set web-server.image.repository="gcr.io/$GCLOUD_PROJECT/data61" --set web-server.image.tag=$TAG --set gateway.image.repository="gcr.io/$GCLOUD_PROJECT/data61" --set gateway.image.tag=$TAG -f $HELM_CONFIG_FILE
